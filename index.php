321a445dcdb7233cf21564d6cc26f7455e8b4340
Christian Hansen <quid@gmx.de>
2013-03-17 22:12:57 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Start</title>
+    </head>
+    <body>
+		<?php
+			echo "test";
+		?>
+    </body>
+</html>
