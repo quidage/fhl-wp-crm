8661d3e3cd54d96a9341165aa695df181be4dcaa
Christian Hansen <quid@gmx.de>
2013-04-17 16:30:52 +0200

    gebe gültige Repository-Methode im Usercontroller an

--- lib/EJC/Controller/UserController.php
+++ lib/EJC/Controller/UserController.php
@@ -15,7 +15,7 @@ class UserController extends AbstractController {
      * @return void
      */
     public function startAction() {
-        $customers = $this->customerRepository->findBId(1);
+        $customers = $this->customerRepository->findById(1);
         var_dump($customers);
         $this->view->assign('title', 'Startseite');
         $this->view->render();
