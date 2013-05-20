c51aef06d4bc710b2b32cf8193ffd6a0260621c1
Christian Hansen <quid@gmx.de>
2013-05-20 16:48:45 +0200

    Starte Session bei Aufruf

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
 
 /**
- * Index file for EJC CRM
+ * Index Datei fuer EJC Kunden-/Projektdatenbank
  * 
  * @author Christian Hansen <christian.hansen@stud.fh-luebeck.de>
  * @package wp-crm
  */
 
-// define constants
+// Starte die Session
+session_start();
+
+// Definiere Konstanten
 define(__AppRoot__, dirname(__FILE__));
 
-// autoloader
+// Lade den Autoloader
 require_once __AppRoot__ . '/lib/loader.php';
 
-// Instantiate request and call action
+// Instanziere den Request
 $request = new \EJC\Request();
 $request->execute();
 
