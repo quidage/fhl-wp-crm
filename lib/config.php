b6c001ab7c18e8c9357df7db13b4241bf355a4bc
Christian Hansen <quid@gmx.de>
2013-06-01 11:34:13 +0200

    DB Config für Christian zu Hause

--- lib/config.php
+++ lib/config.php
@@ -1,15 +1,16 @@
 <?php
 
 /**
- * Cofiguration file
+ * Konfiguration 
  * 
  * @author Christian Hansen <christian.hansen@stud.fh-luebeck.de>
+ * @package wp-crm
  */
 
 $config = array(
     
     // database configuration
-    'db_host' => 'localhost',
+    'db_host' => '192.168.2.20',
     'db_user' => 'wp-crm',
     'db_password' => 'aVtUtzruEn2c8EMT',
     'db_name' => 'wp-crm'
