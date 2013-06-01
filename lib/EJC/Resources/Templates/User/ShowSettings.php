04045775d558820953c6b2031575313de91f2af8
Christian Hansen <quid@gmx.de>
2013-06-01 14:48:42 +0200

    Seite für die Benutzereinstellungen fertiggestellt

--- lib/EJC/Resources/Templates/User/ShowSettings.php
+++ lib/EJC/Resources/Templates/User/ShowSettings.php
@@ -7,12 +7,28 @@
  */
 ?>
 
-<h1>Benutzereinstellungen</h2>
+<h1>Benutzereinstellungen von <?php echo $this->user->getName(); ?></h2>
 
-<?php 
 
-    echo $this->user->getName();
-    echo $this->user->getFirst_name();
-    echo $this->user->getLast_name();
-
-?>
+<table>
+    <thead>
+        <caption>Ihre Daten</caption>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Vorname:</td>
+            <td><?php echo $this->user->getFirst_name(); ?></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Nachname:</td>
+            <td><?php echo $this->user->getLast_name(); ?></td>
+            <td><a href="<?php $this->getUrl('User', 'edit'); ?>" ><img src="images/iconset/writeNew.png" /></a></td>
+        </tr>
+        <tr>
+            <td>E-Mail Adresse:</td>
+            <td><?php echo $this->user->getEmail(); ?></td>
+            <td><a href="<?php $this->getUrl('User', 'edit'); ?>" ><img src="images/iconset/writeNew.png" /></a></td>
+        </tr>
+    </tbody>
+</table>
