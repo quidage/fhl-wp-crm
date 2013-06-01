e3b71a3140b6e74a64151a8cd7a9ed0a4c03fdf5
Christian Hansen <quid@gmx.de>
2013-06-01 16:51:30 +0200

    Formular für die Änderung der Benutzerdaten hinzugefügt

---
+++ lib/EJC/Resources/Templates/User/Edit.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Formular für die Änderung der Benutzerdaten 
+ * 
+ * @author Enrico Lauterschlag <enrico.lauterschlag@web.de>
+ * @package wp-crm
+ */
+?>
+
+<h1>Benutzerdaten ändern</h1>
+
+<form name="edit" method="post" action="<?php $this->getUrl('user', 'edit'); ?>">
+    <input type="hidden" name="user[id]" value="<?php $this->user->getId() ?>">
+    <table>
+        <tr>
+                <td>Vorname:</td>
+                <td><input type="text" name="user[first_name]" value="<?php echo $this->user->getFirst_name(); ?>" /></td>
+        </tr>
+        <tr>
+                <td>Nachname:</td>
+                <td><input type="text" name="user[last_name]" value="<?php echo $this->user->getLast_name(); ?>" /></td>
+        </tr>
+        <tr>
+                <td>E-Mail Adresse:</td>
+                <td><input type="text" name="user[email]" value="<?php echo $this->user->getEmail(); ?>" /></td>
+        </tr>
+        <tr>
+                <td>Altes Passwort:</td>
+                <td><input type="password" name="old_password" /></td>
+        </tr>
+        <tr>
+                <td>Neues Passwort:</td>
+                <td><input type="password" name="user[password]" /></td>
+        </tr>
+        <tr>
+                <td colspan="2" style="text-align: right"><input
+                        type="submit" value="Speichern" class="submit button-link" /></td>
+        </tr>
+    </table>
+</form>
