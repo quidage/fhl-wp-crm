4979986d079ac91fec4944b4e704f0500d2c6efe
Christian Hansen <quid@gmx.de>
2013-06-04 20:46:45 +0200

    neuer DB-Dump

--- lib/wp-crm.sql
+++ lib/wp-crm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.11.1deb1
+-- version 3.5.8.1deb1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Erstellungszeit: 20. Apr 2013 um 16:08
--- Server Version: 5.5.29
--- PHP-Version: 5.4.6-1ubuntu1.2
+-- Host: localhost
+-- Erstellungszeit: 04. Jun 2013 um 20:45
+-- Server Version: 5.5.31-0ubuntu0.13.04.1
+-- PHP-Version: 5.4.9-4ubuntu2
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -43,9 +43,9 @@ CREATE TABLE IF NOT EXISTS `customer` (
 
 INSERT INTO `customer` (`id`, `parent_id`, `cr_date`, `tstamp`, `deleted`, `name`, `street`, `zip`, `city`, `phone`, `fax`, `email`) VALUES
 (1, 1, '0000-00-00 00:00:00', '2013-04-19 14:32:53', 0, 'hansi', 'Straße', '01234', 'Hamburg', '40400231', '', ''),
-(2, 1, '0000-00-00 00:00:00', '2013-04-17 15:43:11', 0, 'Testkunde 2', 'Teststraße 2', '24837', 'Schleswig', '', '', ''),
-(3, 1, '0000-00-00 00:00:00', '2013-04-17 16:03:11', 0, 'Testkunde 1', 'Teststraße 1', '22177', 'Hamburg', '', '', ''),
-(4, 1, '2013-04-17 18:06:45', '2013-04-17 16:06:45', 0, 'Testkunde 1', 'Teststraße 1', '22177', 'Hamburg', '', '', '');
+(2, 2, '0000-00-00 00:00:00', '2013-06-01 12:40:01', 0, 'Testkunde 2', 'Teststraße 2', '24837', 'Schleswig', '', '', ''),
+(3, 2, '0000-00-00 00:00:00', '2013-06-01 12:40:07', 0, 'Testkunde 1', 'Teststraße 1', '22177', 'Hamburg', '', '', ''),
+(4, 2, '2013-04-17 18:06:45', '2013-06-01 17:35:47', 0, 'Testkunde 3', 'Teststraße 1', '22177', 'Hamburg', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -60,9 +60,46 @@ CREATE TABLE IF NOT EXISTS `project` (
   `tstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted` tinyint(1) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `parent_id` (`parent_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Daten für Tabelle `project`
+--
+
+INSERT INTO `project` (`id`, `parent_id`, `cr_date`, `tstamp`, `deleted`, `name`, `description`, `status`) VALUES
+(1, 2, '2013-06-02 00:00:00', '2013-06-01 15:45:19', 0, 'Projekt 1', 'asdfafsdf', 'offen'),
+(2, 2, '2013-06-02 00:00:00', '2013-06-01 12:41:54', 0, 'Projekt 2', 'asdfafsdf', '');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `task`
+--
+
+CREATE TABLE IF NOT EXISTS `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) NOT NULL,
+  `cr_date` datetime NOT NULL,
+  `tstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted` tinyint(1) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Daten für Tabelle `task`
+--
+
+INSERT INTO `task` (`id`, `parent_id`, `cr_date`, `tstamp`, `deleted`, `name`, `description`, `status`) VALUES
+(1, 2, '2013-06-02 00:00:00', '2013-06-01 17:09:08', 0, 'Aufgabe 1', 'asdfafsdf', 'offen'),
+(2, 2, '2013-06-02 00:00:00', '2013-06-01 17:09:05', 0, 'Task 2', 'asdfafsdf', 'offen');
 
 -- --------------------------------------------------------
 
@@ -81,6 +118,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   `last_login` datetime NOT NULL,
   `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `parent_id` (`parent_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
@@ -89,6 +128,6 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Daten für Tabelle `user`
 --
 
-INSERT INTO `user` (`id`, `parent_id`, `cr_date`, `tstamp`, `deleted`, `name`, `password`, `last_login`, `first_name`, `last_name`) VALUES
-(1, 0, '2013-04-16 00:00:00', '2013-04-18 19:20:16', 0, 'hans', 'wurst', '0000-00-00 00:00:00', '', ''),
-(2, 0, '2013-04-19 16:59:43', '2013-04-19 14:59:43', 0, 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', '0000-00-00 00:00:00', '', '');
+INSERT INTO `user` (`id`, `parent_id`, `cr_date`, `tstamp`, `deleted`, `name`, `password`, `last_login`, `first_name`, `last_name`, `admin`, `email`) VALUES
+(1, 0, '2013-04-16 00:00:00', '2013-06-01 12:11:53', 0, 'hans', 'wurst', '0000-00-00 00:00:00', 'Hans', 'Wurst', 0, 'hans.wurst@bockwurstwasser.de'),
+(2, 0, '2013-04-19 16:59:43', '2013-06-01 12:13:45', 0, 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', '0000-00-00 00:00:00', 'Max', 'Mustermann', 1, 'max.mustermann@online.de');
