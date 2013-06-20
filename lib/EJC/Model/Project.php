2538d72ddf0e3360cc029ba149a70d6d846fc619
Christian Hansen <quid@gmx.de>
2013-06-20 22:12:11 +0200

    Neue Eigenschaften Begin und End für Projekt - neuer Dump der DB-Struktur

--- lib/EJC/Model/Project.php
+++ lib/EJC/Model/Project.php
@@ -9,20 +9,34 @@ namespace EJC\Model;
  * @package wp-crm
  */
 class Project extends AbstractModel {
-    
+
     /**
      * Beschreibung
-     * 
+     *
      * @var string
      */
     protected $description;
-    
+
     /**
      * Status
-     * 
+     *
      * @var string
      */
     protected $status;
+
+    /**
+     * Beginn
+     *
+     * @var \DateTime
+     */
+    protected $begin;
+
+    /**
+     * Ende
+     *
+     * @var \DateTime
+     */
+    protected $end;
 
     /**
      * Sorge dafuer, dass die Eigenschaften die richtigen Datentypen bekommen
@@ -30,11 +44,13 @@ class Project extends AbstractModel {
      */
     public function __construct() {
         parent::__construct();
+        $this->begin = new \DateTime($this->begin);
+        $this->end = new \DateTime($this->end);
     }
 
     /**
      * Hole die Beschreibung
-     * 
+     *
      * @return string
      */
     public function getDescription() {
@@ -43,7 +59,7 @@ class Project extends AbstractModel {
 
     /**
      * Setze die Beschreibung
-     * 
+     *
      * @param string $description
      */
     public function setDescription($description) {
@@ -52,7 +68,7 @@ class Project extends AbstractModel {
 
     /**
      * Hole den Status
-     * 
+     *
      * @return string
      */
     public function getStatus() {
@@ -61,31 +77,67 @@ class Project extends AbstractModel {
 
     /**
      * Setze den Status
-     * 
+     *
      * @param string $status
      * @return void
      */
     public function setStatus($status) {
         $this->status = trim($status);
     }
-    
+
     /**
      * Hole alle Tasks zu dem Project
-     * 
+     *
      * @return array
      */
     public function getTasks() {
         $taskRepository = new \EJC\Repository\TaskRepository();
         return $taskRepository->findByParent_id($this->getId());
     }
-    
+
     /**
-     * Hole alle moeglichen Status fuer das Model 
-     * 
+     * Hole alle moeglichen Status fuer das Model
+     *
      * @return array
      */
     public function getPossibleStatus() {
         return array('Offen', 'Geschlossen');
+    }
+
+    /**
+     * Setze den Beginn
+     *
+     * @return \DateTime
+     */
+    public function getBegin() {
+        return $this->begin;
+    }
+
+    /**
+     * Hole den Beginn
+     *
+     * @param \DateTime $begin
+     */
+    public function setBegin(\DateTime $begin) {
+        $this->begin = $begin;
+    }
+
+    /**
+     * Hole das Ende
+     *
+     * @return \DateTime
+     */
+    public function getEnd() {
+        return $this->end;
+    }
+
+    /**
+     * Setze das Ende
+     *
+     * @param \DateTime $end
+     */
+    public function setEnd(\DateTime $end) {
+        $this->end = $end;
     }
 
 }
--- lib/wp-crm.sql
+++ lib/wp-crm.sql
@@ -1,11 +1,17 @@
+-- Struktur der Tabellen
+--
+-- Dump erstellt mit phpmydmin
+--
+-- @created Christian Hansen <christian.hansen@stud.fh-luebeck.de>
+
 -- phpMyAdmin SQL Dump
 -- version 3.5.8.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 09. Jun 2013 um 14:28
+-- Erstellungszeit: 20. Jun 2013 um 22:09
 -- Server Version: 5.5.31-0ubuntu0.13.04.1
--- PHP-Version: 5.4.9-4ubuntu2
+-- PHP-Version: 5.4.9-4ubuntu2.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -37,16 +43,6 @@ CREATE TABLE IF NOT EXISTS `customer` (
   KEY `parent_id` (`parent_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
---
--- Daten für Tabelle `customer`
---
-
-INSERT INTO `customer` (`id`, `parent_id`, `cr_date`, `tstamp`, `deleted`, `name`, `street`, `zip`, `city`, `phone`, `fax`, `email`) VALUES
-(1, 1, '0000-00-00 00:00:00', '2013-04-19 14:32:53', 0, 'hansi', 'Straße', '01234', 'Hamburg', '40400231', '', ''),
-(2, 2, '0000-00-00 00:00:00', '2013-06-01 12:40:01', 0, 'Testkunde 2', 'Teststraße 2', '24837', 'Schleswig', '', '', ''),
-(3, 2, '0000-00-00 00:00:00', '2013-06-01 12:40:07', 0, 'Testkunde 1', 'Teststraße 1', '22177', 'Hamburg', '', '', ''),
-(4, 2, '2013-04-17 18:06:45', '2013-06-01 17:35:47', 0, 'Testkunde 3', 'Teststraße 1', '22177', 'Hamburg', '', '', '');
-
 -- --------------------------------------------------------
 
 --
@@ -62,17 +58,11 @@ CREATE TABLE IF NOT EXISTS `project` (
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `status` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `begin` datetime NOT NULL,
+  `end` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `parent_id` (`parent_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
-
---
--- Daten für Tabelle `project`
---
-
-INSERT INTO `project` (`id`, `parent_id`, `cr_date`, `tstamp`, `deleted`, `name`, `description`, `status`) VALUES
-(1, 2, '2013-06-02 00:00:00', '2013-06-01 15:45:19', 0, 'Projekt 1', 'asdfafsdf', 'offen'),
-(2, 2, '2013-06-02 00:00:00', '2013-06-01 12:41:54', 0, 'Projekt 2', 'asdfafsdf', '');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=21 ;
 
 -- --------------------------------------------------------
 
@@ -93,14 +83,6 @@ CREATE TABLE IF NOT EXISTS `task` (
   KEY `parent_id` (`parent_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
---
--- Daten für Tabelle `task`
---
-
-INSERT INTO `task` (`id`, `parent_id`, `cr_date`, `tstamp`, `deleted`, `name`, `description`, `status`) VALUES
-(1, 2, '2013-06-02 00:00:00', '2013-06-01 17:09:08', 0, 'Aufgabe 1', 'asdfafsdf', 'offen'),
-(2, 2, '2013-06-02 00:00:00', '2013-06-01 17:09:05', 0, 'Task 2', 'asdfafsdf', 'offen');
-
 -- --------------------------------------------------------
 
 --
@@ -120,14 +102,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `admin` tinyint(1) NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `confirm_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `parent_id` (`parent_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
-
---
--- Daten für Tabelle `user`
---
-
-INSERT INTO `user` (`id`, `parent_id`, `cr_date`, `tstamp`, `deleted`, `name`, `password`, `last_login`, `first_name`, `last_name`, `admin`, `email`) VALUES
-(1, 0, '2013-04-16 00:00:00', '2013-06-01 12:11:53', 0, 'hans', 'wurst', '0000-00-00 00:00:00', 'Hans', 'Wurst', 0, 'hans.wurst@bockwurstwasser.de'),
-(2, 0, '2013-04-19 16:59:43', '2013-06-04 20:00:53', 0, 'admin', 'd41d8cd98f00b204e9800998ecf8427e', '2013-06-04 22:00:53', 'Maximus', 'Mustermann', 1, 'max.mustermann@online.de');
