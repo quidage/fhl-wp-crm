0217fd0827018599a44afc030f67435064623e99
Christian Hansen <quid@gmx.de>
2013-06-21 08:35:32 +0200

    fix redirect

--- lib/EJC/Request.php
+++ lib/EJC/Request.php
@@ -154,19 +154,17 @@ class Request {
     public function checkLogin() {
         // Defiere die Actions, fuer welchen kein Login benoetigt wird
         $actionsWithoutNeedForLogin = array(
-            'login', 'showLogin', 'requestNewPassword', 'sendNewPassword'
+            'start', 'login', 'showLogin', 'requestNewPassword', 'sendNewPassword'
         );
 
         if (!in_array($this->action, $actionsWithoutNeedForLogin)) {
             // Wenn User nicht eingeloggt, schicke ihn auf die Login-Seite
             if (!isset($_SESSION['login']) || $_SESSION['login'] < time() - 1800) {
-                if ($this->action !== 'showLogin') {
-                    header('Location: index.php');
-                }
+                header('Location: index.php');
             } else {
-                // Setze den login-Zeitpunkt neu, dass der User wieder 30min hat, bis er
-                // ausgeloggt wird
-                $_SESSION['login'] = time();
+               // Setze den login-Zeitpunkt neu, dass der User wieder 30min hat, bis er
+               // ausgeloggt wird
+               $_SESSION['login'] = time();
             }
         }
     }
