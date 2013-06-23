f85b0a3443b584d96ab7fa3994ba54843afc38f9
unknown <hilbers.julian@gmail.com>
2013-06-23 23:25:52 +0200

    Inhalte aktualisiert

--- lib/EJC/Resources/Templates/Project/Delete.php
+++ lib/EJC/Resources/Templates/Project/Delete.php
@@ -9,8 +9,7 @@
  */
 ?>
 
-<h1>Der Eintrag wurde gelöscht</h1>
+<h1>Eintrag wurde gelöscht</h1>
 
-<p>Der Löschvorgang ist noch nicht ganz fertig :)</p>
 <p>Der Eintrag <i><strong><?php echo $this->projectData->getName(); ?></strong></i> wurde aus dem System entfernt.</p>
 <p><?php $this->getLink('zurück zur Übersicht','User', 'start');?></p>
