1a7fec17c16508291fe36463a1c996d6d894de29
Enrico Lauterschlag <enrico.lauterschlag@web.de>
2013-06-25 20:49:20 +0200

    Anpassungen am Layout

--- lib/EJC/Resources/Templates/Task/Edit.php
+++ lib/EJC/Resources/Templates/Task/Edit.php
@@ -36,8 +36,9 @@
                 </td>
         </tr>
         <tr>
-                <td class="btn-field"><input type="submit" value="Speichern" class="submit button-link" /></td>
-                <td class="btn-field"><input id="std-close-wnd" type="submit" value="Abbrechen" class="submit button-link" /></td>
+                <td></td>
+                <td class="btn-field"><input type="submit" value="Speichern" class="submit button-link" />
+                	<input id="std-close-wnd" type="submit" value="Abbrechen" class="submit button-link" /></td>
         </tr>
     </table>
 </form>
